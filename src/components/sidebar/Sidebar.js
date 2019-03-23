0318d62cac257d92a6a411a72fb56ee671090b18
Ásdís Erla <asdiserla@eduroam.nat.ru.is>
2019-03-23 14:49:58 +0000

    breyta header i sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 
 
 class Header extends Component {
-	render(){
-		return(
-		<div>
-  </div>
-  );
+	render() {
+		return (
+			<div>
+			</div>
+		);
 	}
 }
 
--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
+import Header from '../../components/sidebar/Sidebar'
+import Footer from '../../components/footer/Footer'
 
 //todo hafa token a signin
 class Home extends Component {
-  render() {
-    	return (
-    		<div>
-                <Header/>
-                <Footer/>
-	      </div>
-	    );
-		}
-  
+	render() {
+		return (
+			<div>
+				<Header />
+				<Footer />
+			</div>
+		);
+	}
+
 }
 
 export default Home;
