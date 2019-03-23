1af5e5ec54602ce7323a40491ae42807964b99c2
Flow-flex-the-complex <fth29@hi.is>
2019-03-23 18:59:39 +0000

    login

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -5,37 +5,35 @@ import PropTypes from 'prop-types';
 import './Sidebar.scss';
 
 class Sidebar extends Component {
-	static propTypes = {
-		data: PropTypes.object
-	};
-	render() {
-		const { data } = this.props;
-		function getData() {
-			if (data) {
-				return <h3 className="sidebar__item">{data.name}</h3>;
-			}
-			return <h3 className="sidebar__item">User</h3>;
-		}
-		return (
-			<div className="sidebar">
-				<div className="sidebar__heading">
-					<h3>{getData()}</h3>
-				</div>
-				<a href="/" alt="Home" className="sidebar__item active">
-					Home
-        </a>
-				<Link to="/" alt="Home" className="sidebar__item">
-					My info
+  static propTypes = {
+    data: PropTypes.object
+  };
+  render() {
+    const { data } = this.props;
+    function getData() {
+      if (data) {
+        return <h3 className="sidebar__item">{data.name}</h3>;
+      }
+      return <h3 className="sidebar__item">User</h3>;
+    }
+    return (
+      <div className="sidebar">
+        <div className="sidebar__heading">{getData()}</div>
+        <Link to="/login" alt="Home" className="sidebar__item active">
+          Login
         </Link>
-				<Link to="/" alt="Home" className="sidebar__item">
-					General info
+        <Link to="/" alt="Home" className="sidebar__item">
+          My info
         </Link>
-				<Link to="/" alt="Home" className="sidebar__item">
-					Chat
+        <Link to="/" alt="Home" className="sidebar__item">
+          General info
         </Link>
-			</div>
-		);
-	}
+        <Link to="/" alt="Home" className="sidebar__item">
+          Chat
+        </Link>
+      </div>
+    );
+  }
 }
 
 export default Sidebar;
