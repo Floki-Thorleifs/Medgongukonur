866581bfb9660943fb07d562b3e4ae08b543ef8f
Hugrún Gudmundsdóttir <hugrungudmundsdottir@gestur-416.ru.is>
2019-03-23 17:59:26 +0000

    something

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,25 @@ const baseurl = process.env.REACT_APP_SERVICE_URL;
 
 //todo gera get, delete, fetch, patch, post
 
+export async function get(endpoint) {
+    const token = window.localStorage.getItem('token');
+  
+    const url = `${baseurl}${endpoint}`;
+  
+    const options = {
+      headers: {},
+    };
+  
+    if (token) {
+      options.headers['Authorization'] = `Bearer ${token}`;
+    }
+  
+    const response = await fetch(url, options);
+    const result = await response.json();
+  
+    return { result, status: response.status };
+  }
+
 export default {
  //allt
   };
