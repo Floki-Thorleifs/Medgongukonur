2e336810d4beb8ca5bbad4f9cec658c78112fcd7
Egill Ragnarsson <egill@gestur-127.ru.is>
2019-03-24 06:11:27 +0000

    info site

--- src/components/info/Info.js
+++ src/components/info/Info.js
@@ -7,9 +7,32 @@ import './Info.scss';
 class Info extends Component {
   render() {
     return (
-      <Fragment>
-
-      </Fragment>
+      <div>
+        <h2>Managing & Treating Gestational Diabetes</h2>
+        <h3>How can I manage my gestational diabetes?</h3>
+        <p>Many women with gestational diabetes can manage their blood glucose levels by following a healthy eating plan and being physically active. Some women also may need diabetes medicine.</p>
+        <h3>Follow a healthy eating plan</h3>
+        <p>Your health care team will help you make a healthy eating plan with food choices that are good for you and your baby. The plan will help you know which foods to eat, how much to eat, and when to eat. Food choices, amounts, and timing are all important in keeping your blood glucose levels in your target range.</p>
+        <p>If you’re not eating enough or your blood glucose is too high, your body might make ketones. Ketones in your urine or blood mean your body is using fat for energy instead of glucose. </p>
+        <p>Burning large amounts of fat instead of glucose can be harmful to your health and your baby’s health.</p>
+        <p>Your doctor might recommend you test your urine or blood daily for ketones or when your blood glucose is above a certain level, such as 200. If your ketone levels are high, your doctor may suggest that you change the type or amount of food you eat. Or, you may need to change your meal or snack times.</p>
+        <h3>Be physically active</h3>
+        <p>Physical activity can help you reach your target blood glucose levels. If your blood pressure or cholesterol levels are too high, being physically active can help you reach healthy levels. Physical activity can also relieve stress, strengthen your heart and bones, improve muscle strength, and keep your joints flexible. Being physically active will also help lower your chances of having type 2 diabetes in the future.</p>
+        <p>Talk with your health care team about what activities are best for you during your pregnancy. Aim for 30 minutes of activity 5 days of the week, even if you weren’t active before your pregnancy.2 If you are already active, tell your doctor what you do. Ask your doctor if you may continue some higher intensity activities, such as lifting weights or jogging.</p>
+        <h3>How will I know whether my blood glucose levels are on target?</h3>
+        <p>Your health care team may ask you to use a blood glucose meter to check your blood glucose levels. This device uses a small drop of blood from your finger to measure your blood glucose level. Your health care team can show you how to use your meter.</p>
+        <p>Recommended daily target blood glucose levels for most women with gestational diabetes are </p>
+        <ul>
+          <li>Before meals, at bedtime, and overnight: 95 or less</li>
+          <li>1 hour after eating: 140 or less</li>
+          <li>2 hours after eating: 120 or less</li>
+        </ul>
+        <p>Ask your doctor what targets are right for you.</p>
+        <p>You can keep track of your blood glucose levels using My Daily Blood Glucose Record. You can also use an electronic blood glucose tracking system on your computer or mobile device. Record the results every time you check your blood glucose. Your blood glucose records can help you and your health care team decide whether your diabetes care plan is working. Take your tracker with you when you visit your health care team.</p>
+        <h3>How is gestational diabetes treated if diet and physical activity aren’t enough?</h3>
+        <p>If following your eating plan and being physically active aren’t enough to keep your blood glucose levels in your target range, you may need insulin.</p>
+        <p>If you need to use insulin, your health care team will show you how to give yourself insulin shots. Insulin will not harm your baby and is usually the first choice of diabetes medicine for gestational diabetes. Researchers are studying the safety of the diabetes pills metformin and glyburide during pregnancy, but more long-term studies are needed. Talk with your health care professional about what treatment is right for you.</p>
+      </div>
     );
   }
 }
